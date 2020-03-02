00c7ea12aac07e6e77fb1ca5f26fd772ae46064a
heikki.mustonen@student.oulu.fi <heikki.mustonen@student.oulu.fi>
2020-03-02 11:06:03 +0200

    Updates

---
+++ precache-manifest.ef914c9d416fd10e4b0f02c474c77869.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec359f1a1469ce013de099fd08742766",
+    "url": "/MathPanic/index.html"
+  },
+  {
+    "revision": "88f569a0d0113bc63acf",
+    "url": "/MathPanic/static/css/main.57e35deb.chunk.css"
+  },
+  {
+    "revision": "17a9336f45c23a8abfd1",
+    "url": "/MathPanic/static/js/2.98442b91.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/MathPanic/static/js/2.98442b91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88f569a0d0113bc63acf",
+    "url": "/MathPanic/static/js/main.af3c4be2.chunk.js"
+  },
+  {
+    "revision": "e2062a32b96bcd6e014d",
+    "url": "/MathPanic/static/js/runtime-main.4233cc99.js"
+  }
+]);
